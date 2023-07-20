963cf370a0a6ae5103a8c793733f5a38220aa39a
saannns <76205974+miftahulihsan@users.noreply.github.com>
2023-07-20 13:42:21 +0700

    nambah tekstur pohon

--- FinalProject/Application.cpp
+++ FinalProject/Application.cpp
@@ -995,7 +995,7 @@ void Application::BuildTree() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	int width, height;
-	unsigned char* image = SOIL_load_image("wood1.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
+	unsigned char* image = SOIL_load_image("batang.png", &width, &height, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 1);
